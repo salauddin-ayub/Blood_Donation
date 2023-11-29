28d6b7600ff4c29fbd768837e1aa8364eb4d9440
Ali Akber Mehedi <78915374+aliakbermehedi1@users.noreply.github.com>
2023-11-29 22:19:32 +0600

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,28 @@ import Navbar from "../components/Home/Navbar";
 import CarouselBanner from "../components/Home/CarouselBanner";
 import AboutProject from "../components/Home/AboutProject";
 import ConnectWithUs from "../components/Home/ConnectWithUs";
-import Footer from "./Footer/Footer";
+import Footer from "../components/Home/Footer";
 
 const Home = () => {
   return (
     <div className="">
-      <div><Navbar/></div>
-     <div className=""> <CarouselBanner/></div>
-     <div className=""> <AboutProject/></div>
-     {/* <div><ConnectWithUs/></div> */}
+      <div>
+        <Navbar />
+      </div>
+      <div className="">
+        {" "}
+        <CarouselBanner />
+      </div>
+      <div className="">
+        {" "}
+        <AboutProject />
+      </div>
+      {/* <div><ConnectWithUs/></div> */}
 
-   
-     <div className=""> <Footer/></div>
+      <div className="">
+        {" "}
+        <Footer />
+      </div>
     </div>
   );
 };
