76bf0a6b5742af27e1faa40e6c81ea208eadf394
Zunaid-jony <jony35666666@gmail.com>
2023-12-01 19:16:47 +0600

    tatall reviews

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Home/Navbar";
 import CarouselBanner from "../components/Home/CarouselBanner";
 import Footer from "./Footer/Footer";
+import TotalReviews from "../components/Home/TotalReviews";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
      <div className=""> <CarouselBanner/></div>
 
    
+     <div className=""> <TotalReviews/></div>
      <div className=""> <Footer/></div>
     </div>
   );
