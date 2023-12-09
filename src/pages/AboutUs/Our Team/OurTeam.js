2bb6a95bee7cf866bf25831a1fcae8d5a6397f7b
Zunaid-jony <jony35666666@gmail.com>
2023-12-09 15:42:13 +0600

    OurTeamSections all most

--- src/pages/AboutUs/Our Team/OurTeam.js
+++ src/pages/AboutUs/Our Team/OurTeam.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Navbar from '../../../components/Home/Navbar'
 import CarouselBanner from '../../../components/Home/CarouselBanner'
 import Footer from '../../../components/Home/Footer'
+import OurTeamSections from './OurTeamSections'
 
 const OurTeam = () => {
   return (
     <div>
          <Navbar/>
          <CarouselBanner />
+         <OurTeamSections></OurTeamSections>
+         
          <br></br>
 
 
