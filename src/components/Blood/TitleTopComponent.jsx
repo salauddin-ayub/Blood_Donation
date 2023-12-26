b1576088219a4da3842d51f3fc6d6431eea2541c
Ali Akber Mehedi <78915374+aliakbermehedi1@users.noreply.github.com>
2023-12-26 23:56:09 +0600

    done

--- src/components/Blood/TitleTopComponent.jsx
+++ src/components/Blood/TitleTopComponent.jsx
@@ -1,49 +1,69 @@
-import React from 'react'
+import React from "react";
 import callRedIco from "../../assets/icons/callRedIco.png";
 import Rectangle from "../../assets/icons/Rectangle.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-const TitleTopComponent = ({secondTitle,thirdTitle,fourthTitle}) => {
+const TitleTopComponent = ({ secondTitle, thirdTitle, fourthTitle }) => {
   return (
-    <div className=''>
-       <div className="lg:h-20 md:h-20 h-16 flex justify-between items-center lg:mx-28 md:mx-0 mx-3 lg:mt-0 md:mt-0 mt-2">
-          <div>
-            <p className="font-semibold">
-              Home{" "}
-              <span className="text-brandPrimary px-[2px]">
-                <FontAwesomeIcon icon={faAngleRight} />
-              </span>{" "}
-              {secondTitle}{" "}
-              <span className="text-brandPrimary px-[2px]">
-                <FontAwesomeIcon icon={faAngleRight} />
-              </span>{" "}
-              {thirdTitle}
-              {/* <span className="text-brandPrimary px-[2px]">
+    <div className="">
+      <div className="lg:h-20 md:h-20 h-16 flex justify-between items-center lg:mx-28 md:mx-0 mx-3 lg:mt-0 md:mt-0 mt-2">
+        <div className="lg:text-base md:text-base text-[11px]">
+          <p className="font-semibold">
+            Home{" "}
+            <span className="text-brandPrimary px-[2px]">
+              <FontAwesomeIcon icon={faAngleRight} />
+            </span>{" "}
+            {secondTitle}{" "}
+            <span className="text-brandPrimary px-[2px]">
+              <FontAwesomeIcon icon={faAngleRight} />
+            </span>{" "}
+            {thirdTitle}
+            {/* <span className="text-brandPrimary px-[2px]">
                 <FontAwesomeIcon icon={faAngleRight} />
               </span>{" "} */}
-              {/* {fourthTitle} */}
-            </p>
-          </div>
+            {/* {fourthTitle} */}
+          </p>
+        </div>
 
-          {/* START RIGHT */}
-          <div className="flex justify-end text-xl font-semibold">
-          <img src={Rectangle} alt="Rectengle" />
-            <p className="-ml-[140px] mt-[10px] font-bold text-brandPrimary text-[14px]">
-              Connect With Us
-            </p>
-            <div className="mt-1 ml-6">
-              <div className="rounded-full text-white">
-                <div className="">
-                  <img src={callRedIco} alt="callRedio" className="w-[40px] h-[40px]" />
-                </div>
+        {/* START RIGHT */}
+        {/* start for small */}
+        <div className="flex justify-end mt-2 text-xl font-semibold lg:hidden md:hidden">
+          <img src={Rectangle} alt="" className="w-32 h-10"/>
+          <p className="lg:-ml-[133px] md:-ml-[133px] -ml-[124px] lg:mt-[6px] md:mt-[7px] mt-[6px] font-bold text-brandPrimary lg:text-[14px] md:text-[14px] text-[10px]">
+            Connect With Us
+          </p>
+          <div className="mt-1 lg:ml-3 md:ml-3 ml-10">
+            <div className="rounded-full text-white">
+              <div className="">
+                <img src={callRedIco} alt="" className="w-[30px] h-[30px] hover:rotate-90 hover:shadow-sm hover:rounded-full transition-all duration-500" />
               </div>
             </div>
           </div>
         </div>
-        {/* END top*/}
+        {/* end for small */}
+        {/* start for lasrge */}
+        <div className="lg:flex md:flex justify-end text-xl font-semibold hidden">
+          <img src={Rectangle} alt="Rectengle" />
+          <p className="-ml-[140px] mt-[10px] font-bold text-brandPrimary text-[14px]">
+            Connect With Us
+          </p>
+          <div className="mt-1 ml-6">
+            <div className="rounded-full text-white">
+              <div className="">
+                <img
+                  src={callRedIco}
+                  alt="callRedio"
+                  className="w-[40px] h-[40px]"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* END top*/}
     </div>
-  )
-}
+  );
+};
 
-export default TitleTopComponent
+export default TitleTopComponent;
